374db3361025cb9ff907ebef811bb5bb75f6801b
val.makaveev <val.makaveev@gmail.com>
2018-03-15 17:54:20 +0000

    Introdcution to programming all files

---
+++ C#/CSharp-Part-1/IntroductionToProgramming/IntroductionToProgramming/PrintASequence/PrintASequence.cs
@@ -0,0 +1,33 @@
+﻿/*
+Problem 9.	Print a Sequence
+Write a program that prints the first 10 members of the sequence: 2, -3, 4, -5, 6, -7, ...
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintASequence
+{
+    class PrintASequence
+    {
+        static void Main(string[] args)
+        {
+            int members = 10;
+            int firstMember = 2;
+
+            for (int n = firstMember; n < members + firstMember; n++)
+            {
+                if (n % 2 == 0)
+                {
+                    Console.WriteLine(n);
+                }
+                else
+                {
+                    Console.WriteLine(-n);
+                }
+            }
+        }
+    }
+}
